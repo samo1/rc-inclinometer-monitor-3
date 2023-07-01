e5a71c6658569709ed09059d2faa8dea164c548a
Samuel Valis <samuel.valis@meluzina.sk>
2023-07-01 18:35:27 +0200

    battery indicator icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,10 @@
 #include "pin_config.h"
 #include "TFT_eSPI.h"
 
-#define DISPLAY_HEIGHT 170
-#define DISPLAY_WIDTH 320
-#define DISPLAY_BRIGHTNESS_MAX 252
-#define DISPLAY_BRIGHTNESS_MIN 0
+#define FONT_4 4
 
 TFT_eSPI tft = TFT_eSPI();
-int lcdBacklightBrightness = 128;
+int lcdBacklightBrightness = 120;
 
 void setup() {
     pinMode(PIN_POWER_ON, OUTPUT);
@@ -23,16 +20,32 @@ void setup() {
     ledcAttachPin(38, 0);
     ledcWrite(0, lcdBacklightBrightness);
 
-    tft.setTextSize(2);
     tft.fillScreen(TFT_BLACK);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.drawString("Ahoj", 16, 16, 2);
+    tft.fillRect(0, 0, 320, 36, TFT_DARKGREY);
+
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
+    tft.drawString("Speed", 16, 8, FONT_4);
+
+    tft.fillRect(272, 12, 32, 16, TFT_WHITE);
+    tft.fillRect(304, 16, 4, 8, TFT_WHITE);
+    tft.fillRect(276, 16, 24, 8, TFT_BLACK);
 }
 
 void loop() {
     double batteryVoltage = (analogRead(4) * 2 * 3.3) / 4096;
 
-    tft.drawString(String(batteryVoltage) + " V", 16, 64, 1);
+    double batteryPercentage = (batteryVoltage - 3.7) / (4.2 - 3.7);
+    if (batteryPercentage < 0) {
+        batteryPercentage = 0;
+    }
+    if (batteryPercentage > 1) {
+        batteryPercentage = 1;
+    }
+
+    int batteryIndicatorWidth = static_cast<int>(24 * batteryPercentage);
+    tft.fillRect(276, 16, 24, 8, TFT_BLACK);
+    tft.fillRect(276, 16, batteryIndicatorWidth, 8, TFT_GREEN);
 
     delay(5000);
 }
