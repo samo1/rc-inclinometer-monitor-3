3ebc1d86f7fb4d3dd40e335d505a14d166465524
Samuel Valis <samuel.valis@meluzina.sk>
2023-07-01 17:01:26 +0200

    set brightness and read voltage

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 #include "pin_config.h"
 #include "TFT_eSPI.h"
 
+#define DISPLAY_HEIGHT 170
+#define DISPLAY_WIDTH 320
+#define DISPLAY_BRIGHTNESS_MAX 252
+#define DISPLAY_BRIGHTNESS_MIN 0
+
 TFT_eSPI tft = TFT_eSPI();
+int lcdBacklightBrightness = 128;
 
 void setup() {
     pinMode(PIN_POWER_ON, OUTPUT);
     digitalWrite(PIN_POWER_ON, HIGH);
 
-    tft.init();
+    analogReadResolution(12);
 
+    tft.init();
     tft.setRotation(1);
+
+    ledcSetup(0, 10000, 8);
+    ledcAttachPin(38, 0);
+    ledcWrite(0, lcdBacklightBrightness);
+
     tft.setTextSize(2);
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
@@ -18,5 +30,9 @@ void setup() {
 }
 
 void loop() {
-    delay(1000);
+    double batteryVoltage = (analogRead(4) * 2 * 3.3) / 4096;
+
+    tft.drawString(String(batteryVoltage) + " V", 16, 64, 1);
+
+    delay(5000);
 }
